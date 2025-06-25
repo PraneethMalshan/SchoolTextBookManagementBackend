c0f6a8d59ba5d406770dceda40455918a09d2bf3
PraneethMalshan <praneethmalshan12345@gmail.com>
2025-06-25 21:58:09 +0530

    Init project

---
+++ src/main/java/com/kdpm/school_textbook_management_system/SchoolTextbookManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kdpm.school_textbook_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTextbookManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTextbookManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdpm/school_textbook_management_system/SchoolTextbookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdpm.school_textbook_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolTextbookManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
